d0dc889e7dce7806a4c570635ecd7a4b9b1b4557
BigBoss <pacatung@gmail.com>
2015-04-13 17:15:27 +0800

    g model comment/user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326100011) do
+ActiveRecord::Schema.define(version: 20150413091120) do
+
+  create_table "comments", force: true do |t|
+    t.text     "c_description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "maps", force: true do |t|
     t.string   "name"
@@ -20,6 +26,12 @@ ActiveRecord::Schema.define(version: 20150326100011) do
     t.integer  "money"
     t.integer  "days"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
